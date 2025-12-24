92c33e0bf1c2298fa14ad7141d3bf509d09ba21a
Adil0910 <aadilehtesham63@gmail.com>
2025-12-24 10:59:50 +0530

    commit first

--- config/db.js
+++ config/db.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 
 async function connectDB(){
     try {
-        await mongoose.connect('mongodb://localhost:27017/relationship');
+        await mongoose.connect('mongodb+srv://Adil0910:adil0910@cluster0.icayqlq.mongodb.net/blog');
         console.log("Database connected")
     } catch (error) {
         console.error("Error while conecting to DB", error)
@@ -12,3 +12,4 @@ async function connectDB(){
 
 
 module.exports = connectDB;
+
